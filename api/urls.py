a10b7d832fe321370e08825db775a62567c570ce
Zakariya <pzakariya@mail.ru>
2024-11-15 13:06:51 +0500

    Некоторые изм во View

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from .views import *
 from rest_framework import routers
+import djoser
+from djoser import urls
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 # router = routers.DefaultRouter()
 # router.register(r'users', UserViewSet)
@@ -9,4 +15,8 @@ urlpatterns = [
     path('users/', UserAPIList.as_view(), name='user-list'),
     path('users/<int:pk>/', UserAPIDetail.as_view(), name='user-detail'),
     path('users_del/<int:pk>/', UserAPIDelete.as_view(), name='user-delete'),
+    path('auth/', include(djoser.urls)),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework import status, viewsets
@@ -19,7 +20,8 @@ class UserAPIList(generics.ListCreateAPIView):
 class UserAPIDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
 
 
 class UserAPIDelete(generics.RetrieveDestroyAPIView):
